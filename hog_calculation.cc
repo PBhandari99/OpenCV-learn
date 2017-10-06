821cbab439fb9b784b10dffed707a8445adf3ea8
PBhandari99 <prashantb1097@gmail.com>
2017-10-06 17:46:11 -0400

    first commit

---
+++ hog_calculation.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+
+int main() {
+  // Read image
+  cv::Mat boltImage = cv::imread("images/bolt.png");
+  boltImage.convertTo(boltImage, CV_32F, 1/255.0);
+
+  // calculate gradients gx, gy
+  cv::Mat gx, gy;
+  cv::Sobel(boltImage, gx, CV_32F, 1, 0, 1);
+  cv::Sobel(boltImage, gy, CV_32F, 0, 1, 1);
+
+  // calculate gradient magnitude and direction(in degrees)
+  cv::Mat magnitude, angle;
+  cv::cartToPolar(gx, gy, magnitude, angle, 1);
+
+}
